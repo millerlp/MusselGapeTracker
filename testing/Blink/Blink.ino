5ec0a969bef5641460670707039eb3e5b9716cf4
millerlp <millerlp@gmail.com>
2017-06-18 16:01:57 -0700

    Basic blink test for RevA hardware

---
+++ testing/Blink/Blink.ino
@@ -0,0 +1,27 @@
+/*
+ *  Blink.ino
+ *  
+ *  Simple blink test for the RevA hardware
+ */
+
+
+#define REDLED 4  // on arduino digital pin 4
+#define GRNLED A3
+
+void setup() {
+  pinMode(REDLED, OUTPUT);
+  digitalWrite(REDLED, LOW);
+  pinMode(GRNLED, OUTPUT);
+  digitalWrite(GRNLED, LOW);
+  
+
+}
+
+void loop() {
+  digitalWrite(REDLED, !(digitalRead(REDLED)));
+  delay(100);
+  digitalWrite(GRNLED, !(digitalRead(GRNLED)));
+  delay(100);
+  
+
+}
