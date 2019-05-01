f6dfaa6546aa773a6acd9c91eaeff0809fb2e8cb
millerlp <millerlp@gmail.com>
2019-04-30 22:58:08 -0700

    Working routine, needs refinement
    Functional prototype, can be used to choose 1 of 16 Hall effect channels and write calibration data to SD card. Could use some refinement on the OLED screen display.

--- testing/Hall_tester_MusselGapeTracker_RevC/Hall_tester_MusselGapeTracker_RevC.ino
+++ testing/Hall_tester_MusselGapeTracker_RevC/Hall_tester_MusselGapeTracker_RevC.ino
@@ -1,5 +1,9 @@
 /* Hall_tester_MusselGapeTracker_RevC
  *  Luke Miller 2019
+ *  
+ *  TODO: Update instructions
+ *  TODO: Show output filename somewhere on the screen
+ *  
  *  Meant to work with a MusselGapeTracker Rev C board. Connect
  *  to a KD Scientific Model 200 syringe dispenser to move the 
  *  dispenser back and forth known distances, causing the 
@@ -7,7 +11,7 @@
  *  If you don't have a Model 200 syringe dispenser sitting around,
  *  don't bother running this program.
  *  
- *  Connect Syringe Trigger pin to MusselTracker SCL line (A5)
+ *  Connect Syringe Trigger pin to MusselTracker SCL line (PD3)
  *  Connect Syringe GND to MusselTracker GND
  *  
  *  
@@ -54,56 +58,65 @@
 
 #include "SdFat.h" // https://github.com/greiman/SdFat
 #include <Wire.h>  // built in library, for I2C communications
+#include "SSD1306Ascii.h" // https://github.com/greiman/SSD1306Ascii
+#include "SSD1306AsciiWire.h" // https://github.com/greiman/SSD1306Ascii
 #include <SPI.h>  // built in library, for SPI communications
 #include "RTClib.h" // https://github.com/millerlp/RTClib
 #include <EEPROM.h> // built in library, for reading the serial number stored in EEPROM
-#include "MusselTrackerlib.h"  // https://github.com/millerlp/MusselTrackerlib
+#include "MusselGapeTrackerlib.h" // https://github.com/millerlp/MusselGapeTrackerlib
 
-RTC_Millis rtc; // Create real time clock object (not using onboard DS3231 I2C clock)
+RTC_DS3231 rtc; // Create real time clock object 
 
-
-#define ERRLED 5    // Red error LED pin
-#define GREENLED 6    // Green LED pin
+#define ERRLED 4    // Red error LED pin
+#define GREENLED A3    // Green LED pin
 #define BUTTON1 2     // BUTTON1 on INT0, pin PD2
-#define TRIGGER A5  // Trigger on syringe dispenser
+#define TRIGGER 3  // Trigger on syringe dispenser, pin D3 on RevC
+#define ANALOG_IN A7  // Hall effect analog input from multiplexer, pin ADC7
+#define CS_SD 10    // Chip select for SD card, pin PB2
+#define CS_SHIFT_REG A2 // Chip select for shift registers, pin PC2
+#define SHIFT_CLEAR A1  // Clear (erase) line for shift registers, pin PC1
 
+#define MUX_S0  9   // Multiplexer channel select line, pin PB1
+#define MUX_S1  5   // Multiplexer channel select line, pin PD5
+#define MUX_S2  6   // Multiplexer channel select line, pin PD6
+#define MUX_S3  7   // Multiplexer channel select line, pin PD7
+#define MUX_EN  8   // Multiplexer enable line, pin PB0
 //*************************************
 float moveDist = 0.3175; // units mm, if pump set to dispense 0.1mL
-
+float moveLimit = 12.5; // units millimeters
+float distanceVal = 0; // keep track of distance moved
 //*************
 // Create sd card objects
 SdFat sd;
 SdFile logfile;  // for sd card, this is the file object to be written to
-SdFile calibfile; // for sd card, this is the calibration file to write
-const byte chipSelect = 10; // define the Chip Select pin for SD card
-//*************
-// Define a HallSensor object (from MusselTrackerlib.h library)
-// to access the hall effect sensor functions available in that
-// library. 
-HallSensor hallsensor;
-// Variables to hold hall effect readings
-int hallVal1 = 0;
-int hallVal2 = 0;
-// The HallSensor functions will require an argument to 
-// specify which sensor your want, HALL1 or HALL2, so you 
-// must always specify that in the hall effect sensor
-// function calls. 
-
-
 // Declare initial name for output files written to SD card
-char filename[] = "Hall_CALIB_000_SN00.csv";
+char filename[] = "Hall00_CALIB_00_SN00.csv";
+//*************
+
+//******************************************
+// 0X3C+SA0 - 0x3C or 0x3D for OLED screen on I2C bus
+#define I2C_ADDRESS1 0x3C   // Typical default address
+SSD1306AsciiWire oled1; // create OLED display object, using I2C Wire
+//******************************************
+
 // Placeholder serialNumber
 char serialNumber[] = "SN00";
 // Define a flag to show whether the serialNumber value is real or just zeros
 bool serialValid = false;
 byte debounceTime = 20; // milliseconds to wait for debounce
+int shortPress = 3000;
 volatile bool buttonFlag = false; // Flag to mark when button was pressed
 unsigned long prevMillis;  // counter for faster operations
 unsigned long newMillis;  // counter for faster operations
 unsigned int trialNum = 0; // counter for trial number
-float distanceVal = 0; // keep track of distance moved
+byte channel = 0; // keep track of current hall channel
 DateTime newtime; // keep track of time
-
+char buf[20]; // declare a string buffer to hold the time result
+//******************
+// Create ShiftReg and Mux objects
+ShiftReg shiftReg;
+Mux mux;
+unsigned int newReading = 0; // Reading from Hall effect sensor analog input
 //--------------- buttonFunc --------------------------------------------------
 // buttonFunc
 void buttonFunc(void){
@@ -131,40 +144,116 @@ void setup() {
   digitalWrite(ERRLED, LOW);
   pinMode(GREENLED,OUTPUT);
   digitalWrite(GREENLED, LOW);
-
+  // Set up pin for syringe dispenser drive
+  pinMode(TRIGGER, OUTPUT); // Same as Button2 physical pin
+  digitalWrite(TRIGGER, HIGH);
   // Set up pins for Hall effect sensor
   pinMode(A2, OUTPUT); // sleep line for hall effect 1
   digitalWrite(A2, LOW);
   pinMode(A6, INPUT); // Read the Hall effect sensor on this input
-  // Set up pin for syringe dispenser drive
-  pinMode(TRIGGER, OUTPUT); // Same as SCL pin on Arduino
-  digitalWrite(TRIGGER, HIGH);
+  // Initialize the shift register object
+  shiftReg.begin(CS_SHIFT_REG, SHIFT_CLEAR);
+  // Initialize the multiplexer object
+  mux.begin(MUX_EN,MUX_S0,MUX_S1,MUX_S2,MUX_S3);
 
   Serial.begin(57600);
   Serial.println("Hello");
 
-    // Grab the serial number from the EEPROM memory
+  // Grab the serial number from the EEPROM memory
   // The character array serialNumber was defined in the preamble
   EEPROM.get(0, serialNumber);
   if (serialNumber[0] == 'S') {
     serialValid = true; // set flag
+  } 
+
+  //----------------------------------
+  // Start up the oled display
+  oled1.begin(&Adafruit128x64, I2C_ADDRESS1);
+  Wire.setClock(400000L); //  oled1.set400kHz();  
+  oled1.setFont(Adafruit5x7);    
+  oled1.clear(); 
+  oled1.home();
+  oled1.set2X();
+  // Initialize the real time clock DS3231M
+  Wire.begin(); // Start the I2C library with default options
+  rtc.begin();  // Start the rtc object with default options
+  newtime = rtc.now(); // read a time from the real time clock
+  newtime.toString(buf, 20); 
+  // Now extract the time by making another character pointer that
+  // is advanced 10 places into buf to skip over the date. 
+  char *timebuf = buf + 10;
+  for (int i = 0; i<11; i++){
+    oled1.print(buf[i]);
+  }
+  oled1.println();
+  oled1.println(timebuf);
+
+  if (serialValid){
+    Serial.print(F("Read serial number: "));
+    Serial.println(serialNumber);
+    for (byte i = 0; i<4; i++){
+      oled1.print(serialNumber[i]);
+    }
+    oled1.println();
   } else {
     Serial.print(F("No valid serial number: "));
-    Serial.println(serialNumber);
-    serialValid = false;
+    serialValid = false;    
   }
-    // Initialize the Allegro A1393 hall effect sensors
-  // There is only one hallsensor object, but you feed it
-  // HALL1 or HALL2 as an argument to specify which channel
-  // you want to activate and read. 
-  hallsensor.begin(HALL1);
+
+  delay(2500);
+  //***********************************************
+  // Check that real time clock has a reasonable time value
+  bool stallFlag = true; // Used in error handling below
+  if ( (newtime.year() < 2019) | (newtime.year() > 2035) ) {
+    // There is an error with the clock, halt everything
+    oled1.home();
+    oled1.clear();
+    oled1.set1X();
+    oled1.println(F("RTC ERROR"));
+    oled1.println(buf);
+    oled1.set2X();
+    oled1.println();
+    oled1.println(F("Continue?"));
+    oled1.println(F("Press 1"));
+    Serial.println(F("Clock error, press BUTTON1 on board to continue"));
+
+    bool rtcErrorFlag = true;
+    // Consider removing this while loop and allowing user to plow
+    // ahead without rtc (use button input?)
+    while(stallFlag){
+    // Flash the error led to notify the user
+    // This permanently halts execution, no data will be collected
+      digitalWrite(ERRLED, !digitalRead(ERRLED));
+      delay(100);
+      if (digitalRead(BUTTON1) == LOW){
+        delay(40);  // debounce pause
+        if (digitalRead(BUTTON1) == LOW){
+          // If button is still low 40ms later, this is a real press
+          // Now wait for button to go high again
+          while(digitalRead(BUTTON1) == LOW) {;} // do nothing
+          stallFlag = false; // break out of while(stallFlag) loop
+          oled1.home();
+          oled1.clear();
+        } 
+      }              
+    } // end of while(stallFlag)
+  } else {
+    oled1.home();
+    oled1.clear();
+    oled1.set2X();
+    oled1.println(F("RTC OKAY"));
+    Serial.println(F("Clock okay"));
+    Serial.println(buf); // Print the time to Serial
+    delay(1000);
+  } // end of if ( (newtime.year() < 2019) | (newtime.year() > 2035) ) {
+
   //*************************************************************
-// SD card setup and read (assumes Serial output is functional already)
-  pinMode(chipSelect, OUTPUT);  // set chip select pin for SD card to output
+  // SD card setup and read (assumes Serial output is functional already)
+  pinMode(CS_SD, OUTPUT);  // set chip select pin for SD card to output
   // Initialize the SD card object
   // Try SPI_FULL_SPEED, or SPI_HALF_SPEED if full speed produces
   // errors on a breadboard setup. 
-  if (!sd.begin(chipSelect, SPI_FULL_SPEED)) {
+  if (!sd.begin(CS_SD, SPI_FULL_SPEED)) {
   // If the above statement returns FALSE after trying to 
   // initialize the card, enter into this section and
   // hold in an infinite loop.
@@ -178,21 +267,100 @@ void setup() {
     }
   }
 
-   // Following line sets the RTC to the date & time this sketch was compiled
-   rtc.begin(DateTime(__DATE__, __TIME__));
-   newtime = rtc.now(); // Read time
-  
-  initFileName(newtime); // generate a file name
+//******************************************
+  // Have the user choose which channel to calibrate
+  stallFlag = true;
 
-}
+  // Update oled
+  oled1.home();
+  oled1.clear();
+  oled1.set1X();
+  oled1.println(F("Press button 1"));
+  oled1.println(F("to choose channel"));
+  oled1.println(F("Long press = start"));
+  oled1.set2X();
+  Serial.println(F("Press button 1 to choose channel"));
+  Serial.println(F("Long press = start"));
+
+  long cycleMillis = millis();
+  while (stallFlag){
+    // Take Hall reading and update oled very 200 ms
+    if (millis()-cycleMillis > 200){
+      cycleMillis = millis(); // update
+      // Choose which channel to sample based on channel value
+      // and take a reading to display
+      digitalWrite(MUX_EN, LOW); // enable multiplexer
+      delayMicroseconds(2);
+      shiftReg.shiftChannelSet(channel);
+      mux.muxChannelSet(channel);
+      newReading = readHall(ANALOG_IN);
+      digitalWrite(MUX_EN, HIGH); // disable multiplexer
+  
+      oled1.setCursor(0,3); // Column 0, row 3 (8-pixel rows)
+      oled1.clear(0,128,3,4);
+      oled1.print(F("Ch"));
+      oled1.print(channel);
+      oled1.print(F(": "));
+      oled1.print(newReading); // current Hall value
+      Serial.print(F("Ch"));
+      Serial.print(channel);
+      Serial.print(F(": "));
+      Serial.println(newReading);
+    }
+    // React to a Button1 press
+    if (digitalRead(BUTTON1) == LOW){
+      delay(40);  // debounce pause
+      if (digitalRead(BUTTON1) == LOW){
+        // If button is still low 40ms later, this is a real press
+        // Now wait for button to go high again
+        long buttonMillis = millis();
+        while(digitalRead(BUTTON1) == LOW) {;} // do nothing
+        if (millis() - buttonMillis <= shortPress){
+          // Short press registered, increment channel
+          ++channel;
+          if (channel > 15){
+            channel = 0; // cycle back to 0 when 16 is reached
+          } 
+        } else if (millis() - buttonMillis > shortPress){
+          // Long press registered, break out of while(stallFlag) loop
+          stallFlag = false; // break out of while(stallFlag) loop
+        }
+      } 
+    }
+
+  } // end of while(stallFlag) loop
+
+  newtime = rtc.now();
+  initFileName(newtime);
+  oled1.clear();
+  oled1.home();
+//  oled1.set1X();
+//  oled1.println(F("Press button 1"));
+//  oled1.println(F("to start trial"));
+  oled1.set2X();
+
+} // end of setup loop ************************************
 
 void loop() {
   // Don't collect a round of data until buttonFlag goes true
   newMillis = millis();
   if (newMillis >= prevMillis + 500){
     prevMillis = newMillis; // Update prevMillis to new value
-    Serial.print(F("Ready, Hall1: "));
-    Serial.println(hallsensor.readHall(HALL1));
+    digitalWrite(MUX_EN, LOW); // enable multiplexer
+    delayMicroseconds(2);
+    shiftReg.shiftChannelSet(channel);
+    mux.muxChannelSet(channel);
+    newReading = readHall(ANALOG_IN);
+    digitalWrite(MUX_EN, HIGH); // disable multiplexer
+    Serial.print(F("Value: "));
+    Serial.println(newReading);
+    
+    oled1.setCursor(0,2);
+    oled1.clear(0,128,2,3);
+    oled1.print(F("Ch"));
+    oled1.print(channel);
+    oled1.print(F(": "));
+    oled1.print(newReading);
   }
 
   if (buttonFlag){
@@ -201,36 +369,51 @@ void loop() {
     ++trialNum; // increment trial number
     
     while (distanceVal < 12.5){ 
-      // Initialize the array to hold hall effect sensor values
-      unsigned int hallArray[4] = {0,0,0,0};
-      Serial.print(F("Trial: "));
+
+      Serial.print(F("Hall "));
+      Serial.print(channel);
+      Serial.print(F("\t Trial: "));
       Serial.print(trialNum);
       Serial.print(F("\t Distance "));
       Serial.print(distanceVal,4);
-      Serial.print(F(" mm"));
-      // Read hall effect sensor
-      for (int j = 0; j < 4; j++){
-        // Wake and read the Hall effect sensor
-        hallArray[j] = hallsensor.readHall(HALL1); // This takes an avg of 4 readings
-        Serial.print(F("\t"));
-        Serial.print(hallArray[j]);
-        delay(150);
-      }
-      Serial.println();
-      // Write the readings to the output file
+      Serial.print(F(" mm, Value: "));
+      
+      oled1.setCursor(0,2);
+      oled1.clear(0,128,2,2);
+      oled1.print(F("Ch"));
+      oled1.print(channel);
+      oled1.print(F(": "));
+      oled1.println(newReading);
+      oled1.set1X();
+      oled1.print(F("Trial "));
+      oled1.println(trialNum);
+      oled1.print(F("Distance: "));
+      oled1.println(distanceVal,4);
+      oled1.set2X();
+      
+      // Take a reading 
+      digitalWrite(MUX_EN, LOW); // enable multiplexer
+      delayMicroseconds(2);
+      shiftReg.shiftChannelSet(channel);
+      mux.muxChannelSet(channel);
+      newReading = readHall(ANALOG_IN);
+      digitalWrite(MUX_EN, HIGH); // disable multiplexer
+      
+      Serial.println(newReading);
+      
+      // Write the reading to the output file
         // Reopen logfile. If opening fails, notify the user
       if (!logfile.isOpen()) {
         if (!logfile.open(filename, O_RDWR | O_CREAT | O_AT_END)) {
           digitalWrite(ERRLED, HIGH); // turn on error LED
         }
       }
-      for (int j = 0; j < 4; j++){
-        logfile.print(trialNum);
-        logfile.print(F(","));
-        logfile.print(distanceVal,5);
-        logfile.print(F(","));
-        logfile.println(hallArray[j]);  
-      }
+      logfile.print(channel);
+      logfile.print(trialNum);
+      logfile.print(F(","));
+      logfile.print(distanceVal,5);
+      logfile.print(F(","));
+      logfile.println(newReading);  
       logfile.close();
 
       // Actuate the Trigger pin to make the syringe dispenser
@@ -243,6 +426,12 @@ void loop() {
     
     } // end of while loop 
     Serial.println(F("Finished"));
+    oled1.clear();
+    oled1.home();
+    oled1.set1X();
+    oled1.println(F("Press button 1"));
+    oled1.println(F("to start trial"));
+    oled1.set2X();
     // Reattach the interrupt to allow another trial on this same data file
     attachInterrupt(0, buttonFunc, LOW);
   } 
@@ -251,30 +440,35 @@ void loop() {
 //------------------------------------------------------------------------------
 
 //-------------- initFileName --------------------------------------------------
-// initFileName - a function to create a filename for the SD card based
-// on the 4-digit year, month, day, hour, minutes and a 2-digit counter. 
+// initFileName - a function to create a filename for the SD card. 
 // The character array 'filename' was defined as a global array 
-// at the top of the sketch in the form "Hall_CALIB_000_SN00.csv"
+// at the top of the sketch in the form "Hall00_CALIB_00_SN00.csv"
 void initFileName(DateTime time1) {
-  
+  // Insert the Hall sensor channel number in positions 4-5
+  if (channel < 10){
+    filename[5] = channel + '0';
+  } else if (channel >= 10){
+    filename[4] = (channel % 10)/10 + '0';
+    filename[5] = channel % 10 + '0';
+  }
+
   // If there is a valid serialnumber, insert it into 
-  // the file name in positions 15-18. 
+  // the file name in positions 16-19. 
   if (serialValid) {
     byte serCount = 0;
-    for (byte i = 15; i < 19; i++){
+    for (byte i = 16; i < 20; i++){
       filename[i] = serialNumber[serCount];
       serCount++;
     }
   }
   // Change the counter on the end of the filename
-  // (digits 11,12,13) to increment count for files generated on
+  // (digits 13,14) to increment count for files generated on
   // the same day. This shouldn't come into play
   // during a normal data run, but can be useful when 
   // troubleshooting.
-  for (uint16_t i = 0; i < 1000; i++) {
-    filename[11] = i / 100 + '0'; // 100's digit
-    filename[12] = (i % 100)/10 + '0'; // 10's digit
-    filename[13] = i % 10 + '0'; // 1's digit
+  for (uint16_t i = 0; i < 100; i++) {
+    filename[13] = (i % 100)/10 + '0'; // 10's digit
+    filename[14] = i % 10 + '0'; // 1's digit
     // Check and see if this filename is already on the card
     // and if so, repeat the for loop with a value 1 digit higher
     // until you find a non-existent filename.
@@ -299,7 +493,7 @@ void initFileName(DateTime time1) {
   //------------------------------------------------------------
 
   // write a 2nd header line to the SD file
-  logfile.println(F("Trial,Distance.mm,Hall1"));
+  logfile.println(F("HallChannel,Trial,Distance.mm,Reading"));
   // Update the file's creation date, modify date, and access date.
   logfile.timestamp(T_CREATE, time1.year(), time1.month(), time1.day(), 
       time1.hour(), time1.minute(), time1.second());
