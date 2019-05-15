67fab2f1c05a136cd11ef8234862c2f858ddaee2
Luke Miller <millerlp@gmail.com>
2019-05-15 16:11:31 -0700

    Functional battery monitor
    The RevC silkscreen was originally wrong, R30 shoudl be 15k ohm, and R29 should be 10k, in which case this test sketch then correctly reports battery voltage.

--- testing/Battery_voltage_check/Battery_voltage_check.ino
+++ testing/Battery_voltage_check/Battery_voltage_check.ino
@@ -1,23 +1,20 @@
 /*
  * Battery_voltage_check.ino
  * 
- * **** NON-FUNCTIONAL************
- * ADC7 can't be used as a digital output, so the RevA hardware
- * can't actually check the battery voltage
  * 
- * For MusselGapeTracker RevA hardware
+ * For MusselGapeTracker RevC hardware
  * 
  * Pin ADC6 is the battery voltage sense pin (analog input)
- * Pin ADC7 is used as a digital output to control the battery voltage
- * check circuitry. 
+ * Pin ADC0 is used as a digital output to control the battery voltage
+ * check circuitry. (BATT_MONITOR_EN on the Eagle schematic)
  * 
  * The RevA hardware is initially spec'd with a 10k and 15k resistor
  * divider, so that the input voltage will be divided by 2.5. The system
  * voltage should be 3.3V from the regulator. 
  * 
  */
-#define BATTERY_SENSE A6  // analog input channel to sense battery voltage
-#define BATTERY_CHECK A7 // digital output channel to turn on battery voltage check
+#define BATT_MONITOR A6  // analog input channel to sense battery voltage
+#define BATT_MONITOR_EN A0 // digital output channel to turn on battery voltage check
 
 
 #include <Wire.h>  // built in library, for I2C communications
@@ -35,17 +32,17 @@ SSD1306AsciiWire oled1; // create OLED display object, using I2C Wire
 unsigned long oldMillis;
 int loopTime = 1000; // time between voltage readings
 
-float dividerRatio = 2.5; // Ratio of voltage divider (10k + 15k) / 15k = 2.5
-float refVoltage = 3.3; // Voltage from voltage regulator running ATmega
+float dividerRatio = 2.5; // Ratio of voltage divider (15k + 10k) / 10k = 2.5
+float refVoltage = 3.205; // Voltage from voltage regulator running ATmega
 
 
 void setup() {
-  pinMode(A6, INPUT);
-  pinMode(A7, OUTPUT); 
-  digitalWrite(A7, LOW); // disable battery check initially
+  pinMode(BATT_MONITOR, INPUT);
+  pinMode(BATT_MONITOR_EN, OUTPUT); 
+  digitalWrite(BATT_MONITOR_EN, LOW); // disable battery check initially
 
   //----------------------------------
-  // Start up the oled displays
+  // Start up the oled display
   oled1.begin(&Adafruit128x64, I2C_ADDRESS1);
   oled1.set400kHz();  
   oled1.setFont(Adafruit5x7);    
@@ -60,22 +57,22 @@ void loop() {
     oldMillis = millis();
     // Check battery voltage
     // 1. Enable battery check circuit
-    digitalWrite(A7, HIGH);
+    digitalWrite(BATT_MONITOR_EN, HIGH);
     delay(1);
     // 2. Read the analog input pin
     unsigned int rawAnalog = 0;
-    analogRead(A6);
+    analogRead(BATT_MONITOR);
     delay(3); // Give the ADC time to stablize
     for (byte i = 0; i<4; i++){
-        rawAnalog = rawAnalog + analogRead(A6);
+        rawAnalog = rawAnalog + analogRead(BATT_MONITOR);
         delay(2);
       }
       // Do a 2-bit right shift to divide rawAnalog
       // by 4 to get the average of the 4 readings
       rawAnalog = rawAnalog >> 2;
-//    rawAnalog = analogRead(A6); // Keep this reading
+
     // 3. Shut off the battery voltage sense circuit
-    digitalWrite(A7, LOW);
+//    digitalWrite(BATT_MONITOR_EN, LOW);
     // 4. Convert the rawAnalog count value (0-1024) into a voltage
     float reading = rawAnalog * dividerRatio * refVoltage / 1024;
     // 5. Update OLED screen
@@ -93,6 +90,3 @@ void loop() {
   } // end of if ( (millis() - oldMillis) > loopTime) {
 
 }
-
-
-
